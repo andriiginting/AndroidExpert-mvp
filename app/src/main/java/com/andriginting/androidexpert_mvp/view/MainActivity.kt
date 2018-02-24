8aa511584214edfaaa0d6ff2b5ddcc9b05588554
Andri Ginting <andris.ginting@gmail.com>
2018-02-24 23:48:49 +0700

    Initial commit

---
+++ app/src/main/java/com/andriginting/androidexpert_mvp/view/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.andriginting.androidexpert_mvp.view
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.andriginting.androidexpert_mvp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
